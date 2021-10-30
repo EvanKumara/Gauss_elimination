ef7dc217d33c3a0b1c54b4943c10dbb8a3199ab2
EvanKumara <93023280+EvanKumara@users.noreply.github.com>
2021-10-30 13:29:43 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PratikumMetnum_1
+# gaussElimination
+Python implementation of Gauss Elimination Method.   
+Full mathematical theory and documentation provided in  
+
+How to use this programs:    
+1. Download both .py files  
+2. run run_gauss.py to get immediate result  
+3. edit run_gauss.py as you wish
---
+++ run_gauss.py
@@ -0,0 +1,14 @@
+import gaussElimination
+import numpy as np
+"""
+a = np.array([[4,-2,1,11],
+             [-2,4,-2,-16],
+             [1,-2,4,17]]
+)
+"""
+a = np.array([[4,8,4,80],
+             [2,1,-4,7],
+             [3,-1,2,22]]
+)
+result = gaussElimination.gaussElimination(a)
+print ("the result is" ,result)
